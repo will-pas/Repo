f3e1d7d13a929bc69d4e3b04847b8706e05310b6
will-pas <45357392+will-pas@users.noreply.github.com>
2018-12-07 08:06:21 -0200

    comandos AT pela serial

---
+++ teste_comandos_AT.c
@@ -0,0 +1,36 @@
+﻿#include <SoftwareSerial.h>
+
+
+#define TX_PIN 10
+#define RX_PIN 11
+
+
+SoftwareSerial softSerial(TX_PIN, RX_PIN);
+
+
+void setup(){
+  //Terminal para debug
+  Serial.begin(9600);
+  while (!Serial);
+
+
+  //Comunicacao com o SIM800L
+  softSerial.begin(4800);
+  delay(1000);
+
+
+  Serial.println("Debugging SIM800L!");
+}
+
+
+void loop(){
+  //Print dos dados recebidos pelo modulo
+  if (softSerial.available()){
+    Serial.write(softSerial.read());
+  }
+
+  //Envio de comandos AT para o modulo
+  if (Serial.available()){
+    softSerial.write(Serial.read());
+  }
+}
